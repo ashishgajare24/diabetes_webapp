619f9bb616cccebdf13d019c718fc093a027a4cd
Ashish Gajare <109796273+ashishgajare24@users.noreply.github.com>
2022-11-09 20:13:56 +0530

    Add files via upload
    deploying ml webapp using Streamlit in Heroku

---
+++ diabetes_webapp.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 14:35:41 2022
+
+@author: ashis
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+# loading the saved model
+loaded_model = pickle.load(open('C:/Users/ashis/OneDrive/Desktop/diabetes_webapp/diabetes_model.sav','rb'))
+
+
+# creating a function for Prediction
+
+def diabetes_prediction(input_data):
+    
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+        return 'The person is not diabetic'
+    else:
+        return 'The person is diabetic'
+    
+
+def main():
+    
+    # giving a title
+    st.title('Diabetes Prediction ')
+    st.caption('Made with ❤ by Ashish Gajare', unsafe_allow_html=False)
+    
+    # getting the input data from user
+    
+    Pregnancies = st.text_input('Number of times pregnant')
+    Glucose = st.text_input('Plasma glucose concentration ')
+    BloodPressure = st.text_input('Diastolic blood pressure (mm Hg)')
+    SkinThickness = st.text_input('Triceps skin fold thickness (mm)')
+    Insulin = st.text_input('2-Hour serum insulin (mu U/ml)')
+    BMI = st.text_input('Body mass index (weight in kg/(height in m)^2')
+    DiabetesPedigreeFunction = st.text_input('Diabetes pedigree function')
+    Age = st.text_input('Age (years)')
+    
+    # code for Prediction
+    diagnosis = ''
+    
+    # creating a button for prediction
+    if st.button('Predict'):
+        diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
+        
+        st.success(diagnosis)
+        
+if __name__ == '__main__':
+    main()
